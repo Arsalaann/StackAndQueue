08834527613532c46342fc5ffe8c918230f5c660
Arsalaann <arsalankhanxk@gmail.com>
2023-05-14 14:45:17 +0530

    create queue

--- Queue.cs
+++ Queue.cs
@@ -1,5 +1,24 @@
 namespace StackAndQueue{
     public class Queue{
+
+        int data;
+        Queue next;
+        public Queue(){}
+
+        public Queue(int data){
+            this.data=data;
+        }
+
+        public void EnQueue(int data){
+            this.next=new Queue(data);
+        }
         
+        public void DisplayQueue(){
+            Queue q=this;
+            while(q!=null){
+                Console.WriteLine(q.data+"  ");
+                q=q.next;
+            }
+        }
     }
 }
